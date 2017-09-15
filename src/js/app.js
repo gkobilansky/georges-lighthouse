e590b5e918533c213150e2ec8b87c55e8d6ae5c6
Gene Kobilansky <genek149@gmail.com>
2017-09-15 11:00:52 -0400

    animation tweaks

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ var lighthouse = document.getElementsByClassName('lighthouse-draw');
 
 /* First animation */
 Velocity(lighthouse, { strokeDashoffset: 0 }, {duration: 3000});
-Velocity(lighthouse, { fill: "#145693" }, {delay: 500, queue: false});
-Velocity(george, "fadeIn", { delay: 1000, duration: 1000});
-Velocity(george, { translateY: -75 }, { delay: 1000, duration: 1000,  queue: false });
+Velocity(lighthouse, { fill: "#145693" }, {delay: 1250, queue: false});
+Velocity(george, "fadeIn", { delay: 1500, duration: 1000});
+Velocity(george, { translateY: -75 }, { delay: 1500, duration: 1000,  queue: false });
 Velocity(george, { rotateZ: -5 }, { delay: 1500, duration: 2000, queue: false });
